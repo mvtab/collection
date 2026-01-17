0799f4b3e69111a4e36161460dbb972a62ee4652
mvtab <mvtabilitas@protonmail.com>
2026-01-17 13:58:50 +0100

    Refactored password generators.

--- bash/passwords/gen_pass.sh
+++ bash/passwords/gen_pass.sh
@@ -2,13 +2,15 @@
 
 generate_password() {
 	local COUNT
-	[[ ${#} -eq 0 ]] && COUNT=20 || COUNT=${1}
-	LC_ALL=C tr -dc 'A-Za-z0-9!"#$%&'\''()*+,-./:;<=>?@[\]^_`{|}~' </dev/urandom | head -c "${COUNT}"; echo
+	[[ -n ${1} ]] && COUNT=${1} || COUNT=20
+	shift
+	[[ -n ${1} ]] && REGEX_PATTERN="${1}" || REGEX_PATTERN='[ -~]'
+	cat /dev/urandom | tr -dc "${REGEX_PATTERN}" | head -c "${COUNT}"
 }
 
 generate_weak_password() {
 	local COUNT
 	[[ ${#} -eq 0 ]] && COUNT=20 || COUNT=${1}
-	LC_ALL=C tr -dc 'A-Za-z0-9' </dev/urandom | head -c "${COUNT}"; echo
+	generate_password "${COUNT}" "a-zA-Z0-9"
 }
 
