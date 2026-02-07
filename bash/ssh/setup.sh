911b66110ff67f3da46c29f2661a587781d37a72
mvtab <mvtabilitas@protonmail.com>
2026-02-07 10:56:03 +0100

    Added bashrc ssh startup example.

---
+++ bash/ssh/setup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Example used in .bashrc. 
+# Might work differently or not at all if used independently.
+
+ssh_shutdown() {
+    # pidof is usually in the procps package.
+	for TEMP_AGENT_PID in $(pidof ssh-agent); do
+		eval "$(SSH_AGENT_PID=${TEMP_AGENT_PID} ssh-agent -k)"
+	done
+}
+
+ssh_load_keys() {
+	for SSH_KEY in "${SSH_KEYS_TO_LOAD[@]}"; do
+		ssh-add "${HOME}/.ssh/${SSH_KEY}"
+	done
+}
+
+ssh_startup() {
+	if ps aux | grep -v grep | grep ssh-agent 1> /dev/null; then
+		ssh_shutdown
+	fi
+	eval "$(ssh-agent -s)" 1> /dev/null
+	trap ssh_shutdown EXIT HUP PIPE TERM
+	read -p "Add SSH keys?[y/N]: " -t 10 SHOULD_LOAD_KEYS || echo
+	case "${SHOULD_LOAD_KEYS}" in
+		y|Y|[yY][eE][sS] )
+			ssh_load_keys
+			;;
+		* )
+			;;
+	esac
+}
+
+export SSH_KEYS_TO_LOAD=("ssh-key-1" "ssh-key-2")
+ssh_startup
